38d39ccda73b545f783b781069a2d27b19245c43
Victor Krogh Hansen <kingodk@outlook.dk>
2022-10-11 12:10:38 +0200

    Refactored Parallax effect

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -2,36 +2,19 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    [SerializeField] private Vector3 _parallaxMovementScale;
     [SerializeField] private float _parallaxEffect;
-
+    
     private Transform _cameraTransform;
-
-    private float _length;
-    private float _startPos;
 
     private void Start()
     {
-        _startPos = transform.position.x;
-        _length = GetComponent<SpriteRenderer>().bounds.size.x;
-
         var camera = Camera.main;
         _cameraTransform = camera.transform;
     }
 
     private void FixedUpdate()
     {
-        float temp = (_cameraTransform.position.x * (1 - _parallaxEffect));
-        float distance = (_cameraTransform.position.x * _parallaxEffect);
-
-        transform.position = new Vector3(_startPos + distance, transform.position.y, transform.position.z);
-
-        if (temp > _startPos + _length)
-        {
-            _startPos += _length;
-        }
-        else if (temp < _startPos - _length)
-        {
-            _startPos -= _length;
-        }
+        transform.position = Vector3.Scale(_cameraTransform.position, _parallaxMovementScale);
     }
 }
